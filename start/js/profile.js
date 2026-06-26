6fd8501037b4566145376cad2f5c21c3983c6ce1
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-26 13:07:24 +0200

    Render dei preferiti finito e stilizzato in css con sistema grid

--- start/js/profile.js
+++ start/js/profile.js
@@ -32,4 +32,81 @@ if (account === null) {
 
   hero.appendChild(divImmagine);
   hero.appendChild(divInfo);
+  // render dei preferiti
+  const renderFavs = () => {
+    // funzione per mostrare lista randomica di preferiti
+    // shuffle che randomizza
+    const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
+    // const per i preferiti sul profilo
+    const preferitiRandom = shuffle(getFavourites()).slice(0, 5);
+
+    console.log(preferitiRandom);
+
+    const favsContainer = document.getElementById("profile-favs");
+
+    preferitiRandom.forEach((track, index) => {
+      // creazione elementi preferiti
+      const trackRow = document.createElement("div");
+      trackRow.classList.add("track-row");
+
+      const trackNum = document.createElement("span");
+      trackNum.classList.add("track-num");
+      trackNum.textContent = index + 1;
+
+      trackRow.appendChild(trackNum);
+      favsContainer.appendChild(trackRow);
+
+      const trackCover = document.createElement("img");
+      trackCover.classList.add("track-cover");
+      trackCover.src = track.cover || "https://placehold.co/30x30?text=🎵";
+      trackCover.alt = track.album;
+
+      trackRow.appendChild(trackCover);
+
+      const trackTitleWrap = document.createElement("div");
+      trackTitleWrap.classList.add("track-title-wrap");
+
+      const trackTitle = document.createElement("span");
+      trackTitle.classList.add("track-title");
+      trackTitle.textContent = track.title;
+
+      const trackArtist = document.createElement("span");
+      trackArtist.classList.add("track-artist");
+      trackArtist.textContent = track.artist;
+
+      trackTitleWrap.appendChild(trackTitle);
+      trackTitleWrap.appendChild(trackArtist);
+      trackRow.appendChild(trackTitleWrap);
+
+      const trackAlbum = document.createElement("span");
+      trackAlbum.classList.add("track-album");
+      trackAlbum.textContent = track.album;
+
+      trackRow.appendChild(trackAlbum);
+
+      const trackTime = document.createElement("span");
+      trackTime.classList.add("track-time");
+
+      const minuti = Math.floor(track.durationMs / 60000);
+
+      const secondi = ((track.durationMs % 60000) / 1000).toFixed(0);
+
+      trackTime.textContent = `${minuti}:${secondi.padStart(2, "0")}`;
+
+      trackRow.appendChild(trackTime);
+
+      const trackFav = document.createElement("button");
+      trackFav.classList.add("track-fav", "is-fav");
+      trackFav.textContent = "♥";
+
+      trackFav.addEventListener("click", (e) => {
+        toggleFavourite(track);
+        trackRow.remove();
+      });
+
+      trackRow.appendChild(trackFav);
+    });
+  };
+
+  renderFavs();
 }
